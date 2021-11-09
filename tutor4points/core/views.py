65e2767e405b664ca04f0e1c905477b4b4b672e8
dtuckersuh <suh977@gmail.com>
2021-11-08 17:08:39 -0800

    Add Tutor Requests, Sent Requests components to dashboard

--- tutor4points/core/views.py
+++ tutor4points/core/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, get_user_model, login, forms
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseRedirect
+from .models import TutorRequest
 
 # loginUser handles "/login" endpoint
 # logs in the user that has the specified login credentials from the login form
@@ -51,7 +52,15 @@ def home(request):
     if current_user.is_authenticated:
         users = get_user_model().objects.all().exclude(pk=current_user.id)
         tutors = users.filter(is_tutor=True, school=current_user.school)
-        return render(request, "home.html", {"tutors": tutors})
+        requests_received = TutorRequest.objects.all().filter(tutor=current_user)
+        sent_requests = TutorRequest.objects.all().filter(tutee=current_user)
+        return render(request, "home.html", 
+            {
+                'tutors': tutors, 
+                'requests_received': requests_received,
+                'sent_requests': sent_requests
+            }
+        )
     else:
         return redirect ("")
 
