e3af39485faa99921f02a2af137d32c9ee66d3d4
Thi Nguyen <thivnguyenj@gmail.com>
2021-11-11 15:48:28 -0800

    Add selenium automation test for several test cases
    - Add id to some buttons so that they could be located with selenium

--- tutor4points/core/tests.py
+++ tutor4points/core/tests.py
@@ -1,3 +1,103 @@
-from django.test import TestCase
+from django.test.testcases import LiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from django.contrib.auth import get_user_model
+from .models import School
 
-# Create your tests here.
+class CashOutFormTest (LiveServerTestCase):
+
+    def setUp(self):
+        #add school
+        sjsu = School.objects.create(name="San Jose State University")
+
+        #create test user 1 - tutee
+        get_user_model().objects.create_user (first_name = "tutee", last_name="student", email = "user1@gmail.com", username = "tuteestudent", password = "Testing", is_tutor = True, school = sjsu, is_active = True)
+        #create test user 2 - tutor
+        get_user_model().objects.create_user (first_name = "tutor", last_name="student", email = "user2@gmail.com", username = "tutorstudent", password = "Testing", is_tutor = True, school = sjsu, is_active = True)
+        
+        self.driver = webdriver.Chrome()
+        
+    def tearDown(self):
+        return self.driver.quit() 
+
+    #test inputting negative number into field
+    def test_negative_number (self):
+        self.logintutee()
+        self.driver.find_element_by_id('points-link').send_keys(Keys.RETURN) #go to points page
+        self.transfer_points("-10", "tutor student") #try to transfer -10 points to tutor student
+        assert 'Please enter a positive value' in self.driver.page_source #make sure error message shows up
+
+    #test inputting number of points larger than current points balance
+    def test_number_larger_than_balance (self):
+        self.logintutee()
+        self.driver.find_element_by_id('points-link').send_keys(Keys.RETURN) #go to points page
+        self.transfer_points("100", "tutor student")
+        assert 'Please enter a value less than or equal to your current points balance' in self.driver.page_source #make sure error message shows up
+
+    #test transferring number of points lesser than current points balance
+    def test_transfer_points (self):
+        self.logintutor()
+        self.driver.find_element_by_id('points-link').send_keys(Keys.RETURN) #go to points page
+        self.purchase_points("100") #set tutor points balance to 100
+        self.logout()
+
+        self.logintutee()
+        self.driver.find_element_by_id('points-link').send_keys(Keys.RETURN) #go to points page
+        self.purchase_points("150") #set tutee balance to 150 
+
+        #transfer 150 points from tutor to tutee
+        select = Select(self.driver.find_element_by_id('id_tutors'))
+        select.select_by_visible_text ("tutor student")
+        self.transfer_points ("150", "tutor student")
+        tutee_total_pts = self.driver.find_element_by_id('total-points').text.replace ("\n", " ") #get tutee total points balance
+        assert tutee_total_pts == "0 points"
+        self.logout()
+
+        self.logintutor()
+        self.driver.find_element_by_id('points-link').send_keys(Keys.RETURN)
+        tutor_total_pts = self.driver.find_element_by_id('total-points').text.replace ("\n", " ") #get tutor total points balance
+        assert tutor_total_pts == "250 points"
+
+
+    def logintutee(self):
+        self.driver.get (self.live_server_url + "/login")
+        username_field = self.driver.find_element_by_id('id_username')
+        password_field = self.driver.find_element_by_id('id_password')
+        login_btn = self.driver.find_element_by_id ('login-btn')
+
+        username_field.clear()
+        username_field.send_keys('tuteestudent')
+        password_field.clear()
+        password_field.send_keys('Testing')
+        login_btn.send_keys (Keys.RETURN)
+
+    def logintutor(self):
+        self.driver.get (self.live_server_url + "/login")
+        username_field = self.driver.find_element_by_id('id_username')
+        password_field = self.driver.find_element_by_id('id_password')
+        login_btn = self.driver.find_element_by_id ('login-btn')
+
+        username_field.clear()
+        username_field.send_keys('tutorstudent')
+        password_field.clear()
+        password_field.send_keys('Testing')
+        login_btn.send_keys (Keys.RETURN)
+
+    def logout (self):
+        self.driver.find_element_by_id('logout-btn').send_keys (Keys.RETURN)
+
+    def purchase_points (self, amount):
+        self.driver.find_element_by_id('points-link').send_keys(Keys.RETURN)
+        purchased_pts_field = self.driver.find_element_by_id('id_purchased_points')
+        purchased_pts_field.clear()
+        purchased_pts_field.send_keys (amount)
+        self.driver.find_element_by_id('purchase-btn').send_keys(Keys.RETURN)
+
+    def transfer_points (self, amount, tutor_name):
+        select = Select(self.driver.find_element_by_id('id_tutors'))
+        select.select_by_visible_text (tutor_name)
+        transfer_pts_field = self.driver.find_element_by_id('id_amount_to_transfer')
+        transfer_pts_field.clear()
+        transfer_pts_field.send_keys(amount)
+        self.driver.find_element_by_id('transfer-btn').send_keys(Keys.RETURN)
